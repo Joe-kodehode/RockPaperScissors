85713ca1855340975dec4fab2cf3465272b31448
Joe-kodehode <joe.kodehode@gmail.com>
2023-01-23 23:06:09 +0100

    logic updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,79 @@ import { useState } from "react";
 
 import "./App.css";
 
-const rockAnswer = document.querySelector("#rock-btn");
-const scissorsAnswer = document.querySelector("#scissors-btn");
-const paperAnswer = document.querySelector("#paper-btn");
-
 function App() {
   const possibilities = ["Rock", "Paper", "Scissors"];
   const [result, setResult] = useState("");
   let [score, setScore] = useState(0);
+  const [playerPick, setPlayerPick] = useState();
+  const [compPick, setCompPick] = useState();
 
-  // Function for finding the winner, called when user chooses a button
   const determineWinner = (userAnswer) => {
     let aiAnswer = possibilities[Math.floor(Math.random() * 3)];
-
-    // Logic for deciding winner/loser and returning "win" or "loss"
+    setCompPick(aiAnswer);
+    console.log("AI picked:  " + aiAnswer);
     if (userAnswer === aiAnswer) {
-      result = "Draw";
-    } else if (
-      (userAnswer === "Rock" && aiAnswer === "Scissors") ||
-      (userAnswer === "Scissors" && aiAnswer === "Paper") ||
-      (userAnswer === "Paper" && aiAnswer === "Rock")
-    ) {
-      result = "Win";
-      setScore(score++);
-    } else {
-      result = "Loss";
-      setScore(score--);
+      setResult("Draw");
+    } else if (userAnswer === "Rock" && aiAnswer === "Scissors") {
+      setPlayerPick("Rock");
+      set;
+      setResult("Win");
+      setScore((score += 1));
+    } else if (userAnswer === "Scissors" && aiAnswer === "Paper") {
+      setPlayerPick("Scissors");
+      setResult("Win");
+      setScore((score += 1));
+    } else if (userAnswer === "Paper" && aiAnswer === "Rock") {
+      setPlayerPick("Paper");
+      setResult("Win");
+      setScore((score += 1));
+    } else if (userAnswer === "Scissors" && aiAnswer === "Rock") {
+      setPlayerPick("Scissors");
+      setResult("Loss");
+      setScore((score -= 1));
+    } else if (userAnswer === "Paper" && aiAnswer === "Scissors") {
+      setPlayerPick("Paper");
+      setResult("Loss");
+      setScore((score -= 1));
+    } else if (userAnswer === "Rock" && aiAnswer === "Paper") {
+      setPlayerPick("Rock");
+      setResult("Loss");
+      setScore((score -= 1));
     }
+    console.log("player picked:  " + playerPick);
   };
+  console.log("result:  " + result);
+  console.log("score:  " + score);
 
   return (
     <>
-      <button className="btn" onClick={() => determineWinner("Rock")}>
+      <button
+        id="rock-btn"
+        className="btn"
+        onClick={() => determineWinner("Rock")}
+      >
         Rock
       </button>
-      <button className="btn" onClick={() => determineWinner("Paper")}>
+
+      <button
+        id="paper-btn"
+        className="btn"
+        onClick={() => determineWinner("Paper")}
+      >
         Paper
       </button>
-      <button className="btn" onClick={() => determineWinner("Scissors")}>
+
+      <button
+        id="scissors-btn"
+        className="btn"
+        onClick={() => determineWinner("Scissors")}
+      >
         Scissors
       </button>
+      <p id="score-board">Score: {score}</p>
+      <p>You picked: {playerPick}</p>
+      <p>AI Picked: {compPick}</p>
+      <p>{result === "win" ? "You won" : "You lost"}</p>
     </>
   );
 }
