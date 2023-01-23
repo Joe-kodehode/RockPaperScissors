abd71ce44e1faf0b8957012af111781005489e88
Joe Glendenning <joe.kodehode@gmail.com>
2023-01-23 20:53:14 +0100

    added state for count

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./App.css";
 
 const rockAnswer = document.querySelector("#rock-btn");
@@ -6,35 +8,38 @@ const paperAnswer = document.querySelector("#paper-btn");
 
 function App() {
   const possibilities = ["Rock", "Paper", "Scissors"];
-  let winLossDraw = "";
+  const [result, setResult] = useState("");
+  let [score, setScore] = useState(0);
 
   // Function for finding the winner, called when user chooses a button
-  const victor = (userAnswer) => {
+  const determineWinner = (userAnswer) => {
     let aiAnswer = possibilities[Math.floor(Math.random() * 3)];
 
     // Logic for deciding winner/loser and returning "win" or "loss"
     if (userAnswer === aiAnswer) {
-      winLossDraw = "Draw";
+      result = "Draw";
     } else if (
       (userAnswer === "Rock" && aiAnswer === "Scissors") ||
       (userAnswer === "Scissors" && aiAnswer === "Paper") ||
       (userAnswer === "Paper" && aiAnswer === "Rock")
     ) {
-      winLossDraw = "Win";
+      result = "Win";
+      setScore(score++);
     } else {
-      winLossDraw = "Loss";
+      result = "Loss";
+      setScore(score--);
     }
   };
 
   return (
     <>
-      <button className="btn" onClick={() => victor("Rock")}>
+      <button className="btn" onClick={() => determineWinner("Rock")}>
         Rock
       </button>
-      <button className="btn" onClick={() => victor("Paper")}>
+      <button className="btn" onClick={() => determineWinner("Paper")}>
         Paper
       </button>
-      <button className="btn" onClick={() => victor("Scissors")}>
+      <button className="btn" onClick={() => determineWinner("Scissors")}>
         Scissors
       </button>
     </>
