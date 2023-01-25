3421d089675106d45dd7731b0d005414d86d1d66
Joe Glendenning <joe.kodehode@gmail.com>
2023-01-25 08:54:08 +0100

    bug fixes, cleaned up conditional

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,33 @@ import "./App.css";
 function App() {
   const possibilities = ["Rock", "Paper", "Scissors"];
   const [result, setResult] = useState("");
-  let [score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
   const [playerPick, setPlayerPick] = useState();
   const [compPick, setCompPick] = useState();
 
   const determineWinner = (userAnswer) => {
     let aiAnswer = possibilities[Math.floor(Math.random() * 3)];
     setCompPick(aiAnswer);
-    console.log("AI picked:  " + aiAnswer);
+    setPlayerPick(userAnswer);
+
     if (userAnswer === aiAnswer) {
       setResult("Draw");
-    } else if (userAnswer === "Rock" && aiAnswer === "Scissors") {
-      setPlayerPick("Rock");
+    } else if (
+      (userAnswer === "Rock" && aiAnswer === "Scissors") ||
+      (userAnswer === "Scissors" && aiAnswer === "Paper") ||
+      (userAnswer === "Paper" && aiAnswer === "Rock")
+    ) {
       setResult("Win");
-      setScore((score += 1));
-    } else if (userAnswer === "Scissors" && aiAnswer === "Paper") {
-      setPlayerPick("Scissors");
-      setResult("Win");
-      setScore((score += 1));
-    } else if (userAnswer === "Paper" && aiAnswer === "Rock") {
-      setPlayerPick("Paper");
-      setResult("Win");
-      setScore((score += 1));
-    } else if (userAnswer === "Scissors" && aiAnswer === "Rock") {
-      setPlayerPick("Scissors");
+      setScore(score + 1);
+    } else {
       setResult("Loss");
-      setScore((score -= 1));
-    } else if (userAnswer === "Paper" && aiAnswer === "Scissors") {
-      setPlayerPick("Paper");
-      setResult("Loss");
-      setScore((score -= 1));
-    } else if (userAnswer === "Rock" && aiAnswer === "Paper") {
-      setPlayerPick("Rock");
-      setResult("Loss");
-      setScore((score -= 1));
+      setScore(score - 1);
     }
-    console.log("player picked:  " + playerPick);
   };
-  console.log("result:  " + result);
-  console.log("score:  " + score);
-
   const UserButton = (props) => {
-    return <button onClick={determineWinner(props.name)}>{props.name}</button>;
+    return (
+      <button onClick={() => determineWinner(props.name)}>{props.name}</button>
+    );
   };
 
   return (
@@ -57,7 +42,7 @@ function App() {
       <p id="score-board">Score: {score}</p>
       <p>You picked: {playerPick}</p>
       <p>AI Picked: {compPick}</p>
-      <p>{result === "win" ? "You won" : "You lost"}</p>
+      <p>{result}</p>
     </>
   );
 }
